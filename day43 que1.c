546d9c40d46d8201e968965c94cc4c69516e15c9
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 22:07:43 +0530

    Create day43 que1.c

---
+++ day43 que1.c
@@ -0,0 +1,22 @@
+# Reverse a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, len = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while(str[len] != '\0') {
+        len++;
+    }
+
+    for(i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
